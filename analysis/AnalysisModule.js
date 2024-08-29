05bd2908bee0e8c7e01a4042d311a7ab0fc38207
Mark Nauta <mark.nauta@rabobank.nl>
2024-08-29 14:49:48 +0200

    bugfixes

--- analysis/AnalysisModule.js
+++ analysis/AnalysisModule.js
@@ -15,10 +15,10 @@ module.exports = class AnalysisModule {
     getDateTimeString(){
         let now = new  Date();
         let year = now.getFullYear();
-        let month = ('00'+now.getMonth()+1).slice(-2);
-        let day = ('00'+now.getDate()).slice(2);
-        let hour = ('00'+now.getHours()).slice(2);
-        let minute = ('00'+now.getMinutes()).slice(2);
+        let month = ('00'+(now.getMonth()+1).toString()).slice(-2);
+        let day = ('00'+now.getDate().toString()).slice(2);
+        let hour = ('00'+ now.getHours().toString()).slice(2);
+        let minute = ('00'+now.getMinutes().toString()).slice(2);
         return `${year}${month}${day}${hour}${minute}`;
     }
 
