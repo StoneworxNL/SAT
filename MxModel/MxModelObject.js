3997b0a36c2b6be89233584bfc0c03e17a35a6f7
Mark Nauta <mark.nauta@rabobank.nl>
2025-02-13 15:19:22 +0100

    SAT-MX

--- MxModel/MxModelObject.js
+++ MxModel/MxModelObject.js
@@ -1,5 +1,5 @@
 class MxModelObject {
-   
+
     // static findKey(doc, keyName) {
     //     const obj = Object.keys(doc).find(key => key.toLowerCase() === keyName.toLowerCase());
     //     return obj ? doc[obj] : null;
@@ -10,14 +10,14 @@ class MxModelObject {
         if (keyNames.length === 0) {
             return null;
         }
-    
+
         const keyName = keyNames[0];
         let obj = Object.keys(doc).find(key => key.toLowerCase() === keyName.toLowerCase());
-        if (!obj){
+        if (!obj) {
             obj = keyName;
         }
         const result = doc[obj];
-        if (!result) { return null};
+        if (result === undefined || result === null) { return null; }
         if (keyNames.length === 1) {
             return result;
         } else {
