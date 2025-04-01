c33006ca53ee7e9ff5a10a4d1538983fcd2f82ab
Mark Nauta <mark.nauta@rabobank.nl>
2025-04-01 15:21:15 +0200

    dockerising

--- docker/install_docker.cmd
+++ docker/install_docker.cmd
@@ -6,6 +6,6 @@ if not exist "config" (
 )
 docker login
 
-docker build  --no-cache -t sat:latest .
+docker build --build-arg CACHE_BUST=%CACHE_BUST% -t sat:latest .
 
 docker run --name SAT --volume=config:/usr/src/app/config --volume=output:/usr/src/app/output --workdir=/usr/src/app/ -p 3000:3000 sat:latest
