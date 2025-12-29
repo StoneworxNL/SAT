275f1a071e920de3a71e05d594e7e06d321044f0
m.nauta@stoneworx.nl <m.nauta@stoneworx.nl>
2025-12-29 09:18:31 +0100

    docker build to/from hub

--- docker/build/build_docker.cmd
+++ docker/build/build_docker.cmd
@@ -11,7 +11,7 @@ IF "%MendixProjects%"=="" (
 docker login
 
 docker rm -f SAT 2>nul
-docker build --build-arg CACHEBUST=%CACHEBUST% -t sat:latest -t sat:version%1% .
+docker build --build-arg CACHEBUST=%CACHEBUST% -t mnauta/sat:latest -t mnauta/sat:version%1% .
 
 docker push mnauta/sat:latest
 docker push mnauta/sat:version%1%
--- docker/install_docker.cmd
+++ docker/install_docker.cmd
@@ -12,6 +12,7 @@ if not exist "config" (
 )
 docker login
 
+docker rm -f sat 2>nul
 docker pull mnauta/sat:latest
 
 copy default.json config\default.json
@@ -20,7 +21,6 @@ REM Ensure Docker Desktop has access to your local drive (C:)
 REM This is a manual step in Docker Desktop settings, but we can check if the folders exist
 docker run --restart unless-stopped --name sat -d ^
     -e "MendixProjects=/mendix/" ^
-    -v "%USERPROFILE%\.npmrc:/root/.npmrc" ^
     -v "%cd%\config:/usr/src/app/config" ^
     -v "%cd%\output:/usr/src/output" ^
     -v "%MendixProjects%:/mendix" ^
